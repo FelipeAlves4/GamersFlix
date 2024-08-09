b9622cf13cbbaf5ba959d88c39f352b166f06931
Felipe Alves <felipealvesrodrigues421@gmail.com>
2024-08-09 08:38:12 -0300

    Resolvendo problema no cabeçalho na hora do scron (rolagem), e adicionando o botao top

--- intera.js
+++ intera.js
@@ -1,17 +1,16 @@
-// Button to scroll to top
 var topBtn = document.getElementById("topBtn");
-window.onscroll = function() {scrollFunction()};
-var header = document.getElementById("header");
+        window.onscroll = function() {scrollFunction()};
+        var header = document.getElementById("header");
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        topBtn.style.display = "block";
-        header.classList.add("hidden");
-    } else {
-        topBtn.style.display = "none";
-        header.classList.remove("hidden");
-    }
-}
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                topBtn.style.display = "block";
+                header.classList.add("hidden");
+            } else {
+                topBtn.style.display = "none";
+                header.classList.remove("hidden");
+            }
+        }
 
 topBtn.onclick = function() {
     document.body.scrollTop = 0;
